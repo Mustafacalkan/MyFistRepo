983598d62b15c523127f8989f8a825372d267185
Mustafa Çalkan <mustafa-calkan@hotmail.com>
2023-03-26 19:07:00 +0300

    refactor code clean

--- calculatör/Program.cs
+++ calculatör/Program.cs
@@ -17,26 +17,28 @@ namespace calculatör
 
             Console.Write("İkinci Sayıyı Giriniz: ");
             double sayi = double.Parse(Console.ReadLine());
+            double sonuc = 0;
 
             switch (islem)
             {
                 case "+":
-                    Console.WriteLine(sayi1+sayi);
+                    sonuc = sayi1 + sayi;
                     break;
                 case "-":
-                    Console.WriteLine(sayi1-sayi);
+                    sonuc = sayi1 - sayi;
                     break;
                 case "*":
-                    Console.WriteLine(sayi1*sayi);
+                    sonuc = sayi1 * sayi;
                     break;
                 case "/":
-                    Console.WriteLine(sayi1 / sayi);
+                    sonuc = sayi1 / sayi;
                     break;
                 case "%":
-                    Console.WriteLine(sayi1 % sayi);
+                    sonuc = sayi1 % sayi;
                     break;
 
             }
+            Console.WriteLine(sayi1 + " " + islem + " " + sayi + " = " + sonuc );
 
 
         }
